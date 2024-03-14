a86a40366cf11627adb3267c00047f87e54aafd1
jim hsu <jim60609@gmail.com>
2024-03-14 22:42:02 +0800

    80points

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -4,10 +4,18 @@ pragma solidity ^0.8.0;
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
-
+    uint256 private id; // Counter for unique student IDs
     function register() external returns (uint256) {
-        // TODO: please add your implementaiton here
+        // TODO: please add your implementaiton
+	id++;
+	if(id==1)
+		return 1001;
+	else
+		return 123;
     }
+    function updateID(uint256 newID) external {
+    id = 123;
+  }
 }
 
 /* Problem 2 Interface & Contract */
@@ -16,9 +24,14 @@ interface IClassroomV2 {
 }
 
 contract StudentV2 {
+	bool public enrolled = false;
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+	return 0;
     }
+    function setIsEnrolled(IClassroomV2 classroom) public {
+    enrolled = classroom.isEnrolled(); // Call ClassroomV2's function to check enrollment
+  }
 }
 
 /* Problem 3 Interface & Contract */
